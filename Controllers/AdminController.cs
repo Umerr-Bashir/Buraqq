96f934fa0cb09851d4fd2485cf5edefeaba70ab0
Umerr-Bashir <112628338+Umerr-Bashir@users.noreply.github.com>
2022-09-01 17:11:32 +0500

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Buraqq.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Buraqq.Models.ViewModel;
+
+
+namespace Buraqq.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly BuraqqContext _context;
+
+        public AdminController(BuraqqContext context)
+        {
+            _context = context;
+        }
+
+        [AuthorizeAdmin]
+        public async Task <IActionResult> Index()
+        {
+            return View(await _context.TeacherDetails.Include(model => model.Teacher).Select(x=> new TeacherInfoViewModel {Id=x.Id,Image=x.Image,Fee=x.Fee,Name=$"{x.Teacher.FirstName} {x.Teacher.LastName}",Subject= _context.TeacherSubjects.Include(y=>y.Subject).Where(y=>y.TeacherId==x.TeacherId).Select(y=>y.Subject.Name).ToList() }).ToListAsync()); ;
+        }
+    }
+}
---
+++ Models/ViewModel/TeacherInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Buraqq.Models.ViewModel
+{
+    public class TeacherInfoViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public List<string> Subject { get; set; }= new List<string>();
+        public int Fee { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
